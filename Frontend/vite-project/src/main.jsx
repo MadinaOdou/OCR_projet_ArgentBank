4543bb88c2c8725ee8ddef7e6cc6ff8f9e00a261
Madina <madinaodou@gmail.com>
2023-09-18 14:14:41 +0200

    initialize project using Vite.js

---
+++ Frontend/vite-project/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// import AppRouter from "./components/AppRouter";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      {/* <AppRouter /> */}
+      <Footer />
+    </Router>
+  </React.StrictMode>
+);
